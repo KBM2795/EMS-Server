40bec6c01f3da9f704b6fb6c76928aaafb7efe57
Koshik <koshikmehta2795@gmail.com>
2025-03-15 13:52:50 +0530

    version 2.1

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,8 @@ import authMiddleware from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
-router.post('/login', login )
-router.get('/verify', authMiddleware, verify )
+router.post('/login', login );
+router.get('/verify', authMiddleware, verify );
 
 
 export default router
